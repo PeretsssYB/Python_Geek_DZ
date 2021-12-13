1e8fefffa21e54546fc8ad8ef9dfcbf7cd8590bd
Peretsss <backulinyuriy@gmail.com>
2021-12-13 18:35:54 +0300

    Lesson2_DZ_2.1 Types done

---
+++ Bakulin_Yuriy_dz_2.1.py
@@ -0,0 +1,10 @@
+# Выяснить тип результата выражений:
+from sys import getsizeof
+a = 15 * 3
+b = 15 / 3
+c = 15 // 2
+d = 15 ** 2
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))

4fc466a679cbd983ed46792bc6e08e71f076c4c3
Peretsss <backulinyuriy@gmail.com>
2021-12-23 22:54:56 +0300

    Lesson4_DZ done

---
+++ DZ_4/Bakulin_Yuriy_DZ_4.5.py
@@ -0,0 +1,3 @@
+from currency_rate_get import currency_rates as cur
+
+cur()
---
+++ DZ_4/currency_rate_get.py
@@ -0,0 +1,31 @@
+from requests import get, utils
+from decimal import Decimal
+
+def currency_rates():
+    """Функция получения данных о текущем курсе валюты"""
+
+    val_code = input('Введите код валюты, курс которой хотите узнать: ')
+    target_url = get('http://www.cbr.ru/scripts/XML_daily.asp')
+    encodings = utils.get_encoding_from_headers(target_url.headers)
+    target_content = target_url.content.decode(encoding=encodings)
+    exchange_date = target_content[target_content.find('Date=')+6:target_content.find('name')-2]  # Достаем дату курса
+    key_words = ['Nominal', 'Name', 'Value']  # Список тегов XML для поиска требуемых параметров
+    target_str = target_content[target_content.find(str(val_code).upper()):]  # Поиск валюты в общем списке
+
+    if len(target_str) > 2:
+        target_str = target_str[:target_str.find('</Valute>')]  # Формируем строку с целевой информацией о нужной валюте
+
+        # Формирование списка информации о валюте по тегам XML и приведение их к нужному типу
+
+        currency_info = list(map(lambda x: str(target_str.split(x)[1])[1:-2], key_words))
+        currency_info[0] = int(currency_info[0])
+        # currency_info[2] = float('.'.join(currency_info[2].split(',')))
+        currency_info[2] = Decimal(currency_info[2].replace(',', '.')).quantize(Decimal('0.01'))
+        print(f'Курс актуален на: {exchange_date} \n{currency_info[0]} {currency_info[1]} сегодня меняют на {currency_info[2]} руб.')
+    else:
+        print('Курс валюты не найден/Неккоректный код валюты')
+
+
+
+if __name__ == '__main__':
+    currency_rates()
